0c83275121c525c40498dba4a3518eebbd0d4dea
Migue81 <81migue81@gmail.com>
2024-08-30 17:54:00 +0200

    solo un user en el seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,25 +21,16 @@ class UserSeeder extends Seeder
         $studentRole = Role::firstOrCreate(['name' => 'Estudiante']);
 
         // Crea el usuario de prueba con el rol de Administrador
-        $user = User::factory()->create([
+        $user = User::create([
             'name' => 'Michael Night',
             'email' => 'mNight@gmail.com',
-            'password' => bcrypt('12345678')
+            'password' => bcrypt('12345678') // Asegúrate de que esto use bcrypt
         ]);
 
         // Asigna el rol de Administrador al usuario
         $user->assignRole($adminRole);
 
-        // Rellena la tabla con 49 usuarios más, asignándoles el rol de Estudiante
-        User::factory(49)->create()->each(function ($user) use ($studentRole) {
-            $user->assignRole($studentRole);
-        });
-
-        // Elimina cualquier rol existente y asigna solo el rol de Estudiante a los usuarios, excepto al usuario inicial $user
-        $usersExceptAdmin = User::where('id', '!=', 1)->get(); // Excluding the initial $user (usually with ID 1)
-        foreach ($usersExceptAdmin as $user) {
-            $user->syncRoles([$studentRole]);
-        }
+        // No creamos usuarios adicionales en este seeder, ya que se registrarán sólo a través de la aplicación.
     }
 }
 
