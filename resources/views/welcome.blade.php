1bf65068a6586bae7749c8b9e480ed796810d58c
Migue81 <81migue81@gmail.com>
2024-09-10 19:51:21 +0200

    portada 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <!--El método asset hace referencia a mi carpeta public-->
     <!--Cabecera, Buscador-->
-    <section style="background-image: url({{ asset('img/home/pexelsHome.jpg') }})" class="bg-cover mb-2">
+    <section style="background-image: url({{ asset('img/home/pexelsHome.jpg') }})" class="bg-cover">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
                 <h1 class="text-white font-bold text-4xl">Domina la tecnología web con CodeWeb</h1>
@@ -15,7 +15,7 @@
     </section>
 
     <!--Filtro-->
-    <section class="mb-2 bg-gray-700 py-12">
+    <section class="mb-4 bg-gray-700 py-12">
         <h1 class="text-center text-white text-3xl">¿No sabes que curso te puede interesar?</h1>
         <p class="text-center text-white">
             Dirígete al catálogo de cursos y filtralos por categoría o nivel
@@ -28,7 +28,7 @@
     </section>
 
     <!--Listado últimos cursos-->
-    <section class="mt-2 py-12">
+    <section class="mt-4 py-12">
         <h1 class="text-center text-3xl text-gray-600">Ultimos Cursos</h1>
         <p class="text-center text-gray-500 text-sm mb-6">Siempre estamos subiendo Cursos</p>
 
