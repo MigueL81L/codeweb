104ce60bfb3e7b9b0ac17acf7b69e43e252588ee
Migue81 <81migue81@gmail.com>
2024-09-11 18:28:32 +0200

    p privacidad

--- resources/views/externos/borrador.blade.php
+++ resources/views/externos/borrador.blade.php
@@ -1,49 +1,38 @@
 <x-guest-layout>
-    <div class="min-h-screen bg-gray-800 flex items-center justify-center"> <!-- Fondo gris y centrado -->
-        <div class="bg-white p-6 rounded-lg shadow-lg max-w-2xl w-full border-2"> <!-- Contenedor blanco -->
-            <!-- Encabezado con el logo -->
-            <div class="flex justify-center mb-8">
-                <x-application-mark class="h-10" />
-                <!-- Eliminado el nombre duplicado de la aplicación -->
-            </div>
-
-            <!-- Cuadrícula para contenido -->
-            <div class="grid grid-cols-1 gap-6">
-                <!-- Imagen de contacto -->
-                <div class="flex justify-center mb-4">
-                    <img src="{{ asset('img/externos/contacto.jpg') }}" alt="Contacto" class="rounded-lg shadow-lg max-w-full h-auto" style="max-height: 500px;" />
-                </div>
-
-                <!-- Información de contacto -->
+    <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="pt-4 pb-12 bg-gray-100 dark:bg-gray-900">
+            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                 <div>
-                    <h1 class="text-3xl font-bold mb-4 text-center">Información de Contacto</h1>
-                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-center">Puedes encontrarnos en:</p>
-                    <div class="mt-4">
-                        <div class="flex justify-start mb-2"> <!-- Primera fila: Dirección -->
-                            <span class="font-semibold w-32 text-right pr-4">Dirección:</span>
-                            <p class="text-gray-700 dark:text-gray-300">Calle Ficticia 123, Pontevedra</p>
-                        </div>
-                        <div class="flex justify-start mb-2"> <!-- Segunda fila: Teléfono -->
-                            <span class="font-semibold w-32 text-right pr-4">Teléfono:</span>
-                            <p class="text-gray-700 dark:text-gray-300">612-345-678</p>
-                        </div>
-                        <div class="flex justify-start"> <!-- Tercera fila: Email -->
-                            <span class="font-semibold w-32 text-right pr-4">Email:</span>
-                            <p class="text-gray-700 dark:text-gray-300">
-                                <a href="mailto:mNight@gmail.com" class="text-blue-600 hover:underline">mNight@gmail.com</a>
-                            </p>
-                        </div>
-                    </div>
+                    <x-application-mark class="h-10" />
                 </div>
 
-                <!-- Información sobre Donaciones -->
-                <div class="text-center">
-                    <h2 class="text-2xl font-semibold">Contribuciones Voluntarias</h2>
-                    <p class="mt-2 text-gray-700 dark:text-gray-300">
-                        Si deseas contribuir a nuestro proyecto, puedes hacer una donación voluntaria a través de Bizum. 
-                        Para más detalles, visita nuestra 
+                <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
+                    <h1 class="text-3xl font-bold mb-4 text-center">¿Quiénes Somos?</h1> <!-- Título de sección -->
+                    
+                    <div class="flex justify-center mb-4">
+                        <img src="{{ asset('img/externos/equipo.jpg') }}" alt="Nuestro Equipo" class="rounded-lg shadow-lg max-w-full h-auto" style="max-height: 500px;" />
+                    </div>
+
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        En CodeWeb, somos una academia virtual dedicada a ofrecer formación accesible en programación, bases de datos, y otras disciplinas de la informática. Nuestros cursos están diseñados para todos los niveles, desde principiantes hasta profesionales que buscan mejorar sus habilidades.
+                    </p>
+
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Nuestros instructores son expertos en sus campos, y están comprometidos a brindarte una experiencia de aprendizaje enriquecedora, práctica y eficaz. Creemos en el poder de la educación online y en su capacidad para flexibilizar tu aprendizaje, adaptándose a tu ritmo y horario.
+                    </p>
+
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Te proponemos que entres y le eches un vistazo a la página, y a los cursos de los que dispondrás. Siempre estamos subiendo nuevos cursos. Puedes matricularte en tantos como quieras, y lo mejor de todo es que puedes hacerlo Gratis!. 
+                    </p>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left font-bold">
+                        Eso si, si consideras que hacemos cursos de valor, agradeceríamos una donación, para ayudarnos a crecer, y atender a la cada día mayor comunidad de estudiantes que tenemos.
+                    </p>
+                    
+                    <p>
                         <a href="{{ route('payment-address') }}" class="text-blue-600 hover:underline">Dirección de Pago</a>.
                     </p>
+
+
                 </div>
             </div>
         </div>
--- resources/views/externos/privacy-policy.blade.php
+++ resources/views/externos/privacy-policy.blade.php
@@ -1,6 +1,65 @@
 <x-guest-layout>
-    <div class="container mx-auto py-4">
-        <h1 class="text-2xl font-bold">Política de Privacidad</h1>
-        <p>Contenido de la política de privacidad aquí...</p>
+    <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="pt-4 pb-12 bg-gray-100 dark:bg-gray-900">
+            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+                <div>
+                    <x-application-mark class="h-10" />
+                </div>
+
+                <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
+                    <h1 class="text-3xl font-bold mb-4 text-center">Política de Privacidad</h1> <!-- Título de sección -->
+
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        En CodeWeb, valoramos y respetamos la privacidad de nuestros usuarios. Esta Política de Privacidad explica cómo recopilamos, utilizamos y compartimos la información personal de los usuarios que acceden a nuestra plataforma.
+                    </p>
+
+                    <h2 class="text-2xl font-bold mt-4">Información que recopilamos</h2>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Recopilamos información personal cuando los usuarios se registran en nuestra plataforma, como:
+                    </p>
+                    <ul class="list-disc list-inside mt-2 text-gray-700 dark:text-gray-300">
+                        <li>Nombre completo</li>
+                        <li>Correo electrónico</li>
+                        <li>Fecha de nacimiento</li>
+                    </ul>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        También recopilamos información sobre el uso de la plataforma, incluyendo datos de navegación y preferencias de usuario.
+                    </p>
+
+                    <h2 class="text-2xl font-bold mt-4">Uso de la información</h2>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Usamos la información recopilada para:
+                    </p>
+                    <ul class="list-disc list-inside mt-2 text-gray-700 dark:text-gray-300">
+                        <li>Facilitar el acceso a nuestros cursos.</li>
+                        <li>Brindar soporte al usuario.</li>
+                        <li>Notificar sobre cambios y actualizaciones.</li>
+                        <li>Enviar comunicados y ofertas relevantes.</li>
+                        <li>Mejorar continuamente nuestros servicios y la experiencia del usuario.</li>
+                    </ul>
+
+                    <h2 class="text-2xl font-bold mt-4">Divulgación de la información</h2>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        No compartimos su información personal con terceros, salvo que sea necesario para cumplir con la ley, o si tenemos su consentimiento.
+                    </p>
+
+                    <h2 class="text-2xl font-bold mt-4">Seguridad de la información</h2>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Implementamos medidas de seguridad adecuadas para proteger la información personal de nuestros usuarios. Sin embargo, ningún método de transmisión por internet o método de almacenamiento electrónico es 100% seguro.
+                    </p>
+
+                    <h2 class="text-2xl font-bold mt-4">Cambios a esta política</h2>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Nos reservamos el derecho a modificar esta Política de Privacidad en cualquier momento. Cualquier cambio será publicado en esta página con una nueva fecha de vigencia.
+                    </p>
+
+                    <h2 class="text-2xl font-bold mt-4">Contacto</h2>
+                    <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
+                        Si tiene alguna pregunta sobre esta política, o si desea ejercer sus derechos de privacidad, contáctenos a través del correo electrónico proporcionado en nuestra plataforma.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </x-guest-layout>
+
