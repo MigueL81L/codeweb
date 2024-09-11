7cea56283b34e804ae92a11c5402aab9bd330844
Migue81 <81migue81@gmail.com>
2024-09-11 09:25:35 +0200

    logo y nombre en la página de registro

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,5 @@
-<a href="{{ route('register') }}">
+<a href="{{ route('register') }}" class="flex items-center space-x-2">
     <img src="{{ asset('img/logo/Logo64.png') }}" alt="Logo" style="width: 64px; height: 64px;">
+    <span class="text-xl font-semibold text-gray-800 font-serif">CodeWeb</span>
 </a>
+
