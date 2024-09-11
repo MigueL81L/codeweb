6ae904c1b694319642f2ea4261f8f2a7f54f9b57
Migue81 <81migue81@gmail.com>
2024-09-11 17:26:34 +0200

    probando 5

--- resources/views/externos/contact.blade.php
+++ resources/views/externos/contact.blade.php
@@ -1,85 +1,52 @@
-
 <x-guest-layout>
-<div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
-    <div class="pt-4 pb-12 bg-gray-100 dark:bg-gray-900">
-        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
-            <div>
-                <x-application-mark class="h-10" />
-            </div>
-
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
-                <div class="flex justify-center mb-4">
-                    <img src="{{ asset('img/externos/contacto.jpg') }}" alt="Contacto" class="rounded-lg shadow-lg max-w-full h-auto" style="max-height: 500px;" />
+    <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="pt-4 pb-12 bg-gray-100 dark:bg-gray-900">
+            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+                <div>
+                    <x-application-mark class="h-10" />
                 </div>
 
-<h2>¿Qué información recopilamos?</h2>
-Codersfree recopila el siguiente tipo de información:
-<ol>
-<li>
-Los datos que nos envías al momento de registrarte en nuestra plataforma o al tratar de comunicarte con nosotros. Por ejemplo: el correo electrónico, fechas de nacimiento, nombres completos, entre otros.
-</li>
-<li>
-Información sobre las páginas de la empresa a la que accedes, en este caso serían los datos sobre tu dispositivo: sistema operativo o tipo de navegador.
-</li>
-<li>
-Recolectaremos tus datos de usuario en cuanto al contenido compartido dentro de nuestra plataforma, las páginas de Codersfree a las que se accede, los datos de cursos (comportamiento, uso del producto, comentarios, proyectos creados), datos de pago y datos sobre las compras.
-</li>
-</ol>
-Es importante destacar que nuestra tecnología nos permite identificar datos como la ubicación geográfica del usuario (país y ciudad) e historial de navegación por nuestro sitio web y aplicación. Esto en caso de reportar un problema y tengamos la necesidad de ver cuáles páginas ha usado el usuario e identificar mejor su caso.
-<p>Por motivos de seguridad no almacenamos los datos confidenciales como el titular de una tarjeta, entiéndase los números de tarjeta de crédito o datos de autenticación de la misma; datos de los usuarios y sus cuentas en otros servicios, salvo se encuentren directamente vinculados a Codersfree.</p>
-<p>Al realizar un pago a través de alguna de nuestras pasarelas de pago usted aceptará la política de privacidad correspondiente a dicha pasarela. Codersfree no tiene acceso a ninguno de los datos que el usuario comparta con la entidad bancaria por la que decida realizar el pago.</p>
-<p>El usuario tiene la opción de añadir información a su perfil, nos referimos a incluir una foto de perfil, una breve descripción, país y ciudad donde vive o proviene y fecha de nacimiento.</p>
-<h2>Divulgación de tu información</h2>
-Codersfree utilizará la información recopilada para:
-<ol>
-<li>Proporcionar información que el usuario facilite.</li>
-<li>Notificar al usuario sobre cambios en la plataforma.</li>
-<li>Permitir al usuario participar en actividades interactivas de la plataforma.</li>
-<li>Brindar soporte al usuario.</li>
-<li>Validar la veracidad de la información brindada.</li>
-<li>Atender las consultas y solicitudes del usuario.</li>
-<li>Gestionar las preferencias del usuario referente a su cuenta.</li>
-<li>Realizar estudios estadísticos para potenciar el funcionamiento de los servicios prestados por Codersfree y lograr mejorar la experiencia de usuario, así como tendencias, tráfico y realizar seguimiento de compras.</li>
-<li>Garantizar el cumplimiento de los protocolos de seguridad y otros aspectos relacionados a los servicios brindados por Codersfree.</li>
-<li>Facilitar el funcionamiento técnico de los servicios, lo cual incluye solución de problemas, seguridad de los servicios, prevención de fraude y uso indebido del contenido de la empresa.</li>
-<ol>
-Codersfree no divulga con ningún tercero tu información, pues su uso está restringido únicamente a nuestro servicio. Asimismo, debemos acotar que los datos recopilados serán recopilados para las siguientes situaciones:
-<ol>
-<li>
-<b>Administración del sistema:</b> Podemos usar los datos personales del usuario para la administración de nuestros sistemas, el diagnóstico de problemas con los servidores, supervisión del rendimiento, tráfico en nuestro sitio web y aplicación y obtener información demográfica.
-</li>
-<li>
-<b>Administración del sistema:</b> La información personal del usuario será usada para enviar a su correo nuestros anuncios, ofertas y publicidad sobre los productos ofrecidos por la plataforma. El usuario expresará su total consentimiento de recibir este tipo de contenido, ya que al registrarse puede activar en el "check box" la opción "Acepto recibir material publicitario y/o comercial de Codersfree". Si luego el usuario quiere desistir de este servicio puede oprimir la opción "Darse de baja".
-</li>
-<li>
-<b>Encuestas:</b> A través del correo electrónico el usuario o cualquier otro medio disponible el usuario recibirá encuestas sobre nuestros servicios y/o cualquier otro tema de interés con el fin de mejorar nuestros servicios.
-</li>
-<li>
-<b>Facturación:</b> Usaremos los datos personales del usuario con el fin de crear un registro y un historial de las compras realizadas a la empresa.
-</li>
-</ol>
-<h2>Eliminación de datos</h2>
-<p>En concordancia con las regulaciones de Facebook para Apps y sitios web, debemos brindarle a los usuarios las instrucciones para eliminar sus datos. Si quieres eliminar tu actividad relacionada a Coders Free, lo puedes hacer de la siguiente manera.</p>
-<ol>
-<li>Ingresa a tu cuenta de Facebook y haz clic en “Configuración y privacidad”. Después haz clic en “Configuración”. </li>
-<li>Ve a la sección de “Apps y sitios web”, aquí podrás ver toda tu actividad relacionada a aplicaciones y páginas web registradas en tu cuenta de Facebook. 
-<img src="https://codersfree.com/img/2022-07-16.png" alt="">
-</li>
-<li>Selecciona la casilla correspondiente a Codersfree y haz clic en “Eliminar”. 
-<img src="https://codersfree.com/img/2022-07-16%20(1).png" alt="">
-</li>
-<li>Selecciona las casillas de acuerdo a tu preferencia y haz clic en “Eliminar”. 
-<img src="https://codersfree.com/img/2022-07-16%20(2).png" alt="">
-</li>
-<li>¡Listo! Eliminaste a Coders Free de tus actividades de manera exitosa.</li>
-</ol>
-Codersfree responde a cualquier duda que se tenga sobre la Política de Privacidad. Envíenos un correo a contacto@codersfree.com y comuníquese con nosotros.
+                <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
+                    <div class="flex justify-center mb-4">
+                        <img src="{{ asset('img/externos/contacto.jpg') }}" alt="Contacto" class="rounded-lg shadow-lg max-w-full h-auto" style="max-height: 500px;" />
+                    </div>
 
+                    <div>
+                        <h1 class="text-3xl font-bold mb-4 text-center">Información de Contacto</h1>
+                        <p class="mt-2 text-gray-700 dark:text-gray-300 text-center">Puedes encontrarnos en:</p>
+                        <div class="mt-4">
+                            <div class="flex justify-start mb-2"> <!-- Primera fila: Dirección -->
+                                <span class="font-semibold w-32 text-right pr-4">Dirección:</span>
+                                <p class="text-gray-700 dark:text-gray-300">Calle Ficticia 123, Pontevedra</p>
+                            </div>
+                            <div class="flex justify-start mb-2"> <!-- Segunda fila: Teléfono -->
+                                <span class="font-semibold w-32 text-right pr-4">Teléfono:</span>
+                                <p class="text-gray-700 dark:text-gray-300">612-345-678</p>
+                            </div>
+                            <div class="flex justify-start"> <!-- Tercera fila: Email -->
+                                <span class="font-semibold w-32 text-right pr-4">Email:</span>
+                                <p class="text-gray-700 dark:text-gray-300">
+                                    <a href="mailto:mNight@gmail.com" class="text-blue-600 hover:underline">mNight@gmail.com</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Información sobre Donaciones -->
+                    <div class="text-center">
+                        <h2 class="text-2xl font-semibold">Contribuciones Voluntarias</h2>
+                        <p class="mt-2 text-gray-700 dark:text-gray-300">
+                            Si deseas contribuir a nuestro proyecto, puedes hacer una donación voluntaria a través de Bizum. 
+                            Para más detalles, visita nuestra 
+                            <a href="{{ route('payment-address') }}" class="text-blue-600 hover:underline">Dirección de Pago</a>.
+                        </p>
+                    </div>
+
+
+                </div>
             </div>
         </div>
     </div>
-        </div>
-
 </x-guest-layout>
 
 
