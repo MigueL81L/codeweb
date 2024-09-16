8bd60aa0b3017ad2469efdc3146084d72f361764
Migue81 <81migue81@gmail.com>
2024-09-16 17:41:30 +0200

    paginación 7

--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -1,58 +1,51 @@
-<div>
+<div class="bg-gray-200 py-4 mb-16">  
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex"> 
+        <div class="grid grid-cols-5 gap-x-4">
+            <!-- Botón para resetear los filtros -->
+            <div class="col-span-1">
+                <div class="flex items-center justify-start space-x-4">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            wire:click="resetFilters">
+                        <i class="fas fa-archway text-xs mr-2"></i>  
+                        Todos los Cursos
+                    </button>
+                </div>
+            </div>
 
-    <div class="bg-gray-200 py-4 mb-16">  
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex"> 
-            <div class="grid grid-cols-5 gap-x-4">
-                
-                <!-- Botón para resetear los filtros --> 
-                <div class="col-span-1">
-                    <div class="flex items-center justify-start space-x-4">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                wire:click="resetFilters">
-                            <i class="fas fa-archway text-xs mr-2"></i>  
-                            Todos los Cursos
-                        </button>
+            <!-- Filtro por Categoría -->
+            <div class="col-span-2">
+                <form wire:submit.prevent="filterCategories" class="flex items-center justify-end space-x-4">
+                    <div>
+                        <select wire:model="selectedCategory" id="selectedCategories" name="selectedCategories" class="h-10 border-gray-300 rounded-lg">
+                            <option value="">Seleccione una Categoría</option>
+                            @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
-                </div>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <i class="fas fa-th-list text-xs mr-2"></i>  
+                        Filtrar por Categorías
+                    </button>
+                </form>
+            </div>
 
-                <!-- Filtro por Categoría -->
-                <div class="col-span-2">
-                    <form wire:submit.prevent="filterCategories" class="flex items-center justify-end space-x-4">
-                        <div>
-                            <select wire:model="selectedCategories" id="selectedCategories" name="selectedCategories" class="h-10 border-gray-300 rounded-lg">
-                                <option class="py-2" value="">Seleccione una Categoría</option>
-                                @foreach($categories as $category)
-                                    <option value="{{$category->id}}">{{$category->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <i class="fas fa-th-list text-xs mr-2"></i>  
-                            Filtrar por Categorías
-                        </button>
-                    </form>
-                </div>
-
-                <!-- Filtro por Nivel -->
-                <div class="col-span-2">
-                    <form wire:submit.prevent="filterLevels" class="flex items-center justify-end space-x-4">
-                        <div>
-                            <select wire:model="selectedLevels" id="selectedLevels" name="selectedLevels" class="h-10 border-gray-300 rounded-lg">
-                                <option class="py-2" value="">Seleccione el Nivel</option>
-                                @foreach($levels as $level)
-                                    <option value="{{$level->id}}">{{$level->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <i class="fas fa-filter text-xs mr-2"></i>  
-                            Filtrar por Niveles
-                        </button>
-                    </form>
-                </div>
-
+            <!-- Filtro por Nivel -->
+            <div class="col-span-2">
+                <form wire:submit.prevent="filterLevels" class="flex items-center justify-end space-x-4">
+                    <div>
+                        <select wire:model="selectedLevel" id="selectedLevels" name="selectedLevels" class="h-10 border-gray-300 rounded-lg">
+                            <option value="">Seleccione el Nivel</option>
+                            @foreach($levels as $level)
+                                <option value="{{ $level->id }}">{{ $level->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <i class="fas fa-filter text-xs mr-2"></i>  
+                        Filtrar por Niveles
+                    </button>
+                </form>
             </div>
         </div>
     </div>
@@ -65,15 +58,15 @@
             @endforeach
         </div>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4 mb-8">
-            {{ $courses->links('pagination::tailwind') }}
+            {{ $courses->links() }}
         </div>
     @else
         <div class="bg-gray-100 rounded-lg p-4 text-center w-full">
             <p class="text-gray-500 font-bold block">{{ $mensaje }}</p>
         </div>
     @endif
-
 </div>
+
 
 
 
