ed54c64b6d9f4148701ac2dd137ccd21336c2636
Migue81 <81migue81@gmail.com>
2024-09-16 18:07:36 +0200

    paginación 10

--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -1,5 +1,4 @@
 <div>
-
     <div class="bg-gray-200 py-4 mb-16">  
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex"> 
             <div class="grid grid-cols-5 gap-x-4">
@@ -17,42 +16,27 @@
 
                 <!-- Filtro por Categoría -->
                 <div class="col-span-2">
-                    <form wire:submit.prevent="filterCategories" class="flex items-center justify-end space-x-4">
-                        <div>
-                            <select wire:model="selectedCategories" id="selectedCategories" name="selectedCategories" class="h-10 border-gray-300 rounded-lg">
-                                <option class="py-2" value="">Seleccione una Categoría</option>
-                                @foreach($categories as $category)
-                                    <option value="{{$category->id}}">{{$category->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <i class="fas fa-th-list text-xs mr-2"></i>  
-                            Filtrar por Categorías
-                        </button>
-                    </form>
+                    <div>
+                        <select wire:model="selectedCategories" wire:change="updatedSelectedCategories" id="selectedCategories" name="selectedCategories" class="h-10 border-gray-300 rounded-lg">
+                            <option class="py-2" value="">Seleccione una Categoría</option>
+                            @foreach($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
 
                 <!-- Filtro por Nivel -->
                 <div class="col-span-2">
-                    <form wire:submit.prevent="filterLevels" class="flex items-center justify-end space-x-4">
-                        <div>
-                            <select wire:model="selectedLevels" id="selectedLevels" name="selectedLevels" class="h-10 border-gray-300 rounded-lg">
-                                <option class="py-2" value="">Seleccione el Nivel</option>
-                                @foreach($levels as $level)
-                                    <option value="{{$level->id}}">{{$level->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <i class="fas fa-filter text-xs mr-2"></i>  
-                            Filtrar por Niveles
-                        </button>
-                    </form>
+                    <div>
+                        <select wire:model="selectedLevels" wire:change="updatedSelectedLevels" id="selectedLevels" name="selectedLevels" class="h-10 border-gray-300 rounded-lg">
+                            <option class="py-2" value="">Seleccione el Nivel</option>
+                            @foreach($levels as $level)
+                                <option value="{{$level->id}}">{{$level->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -72,8 +56,8 @@
             <p class="text-gray-500 font-bold block">{{ $mensaje }}</p>
         </div>
     @endif
-
 </div>
+
 
 
 
