f210da1948634485aa9c1e73b0104f36c86abefc
Migue81 <81migue81@gmail.com>
2024-09-18 19:48:11 +0200

    todo el input 2

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,10 +1,10 @@
-<form class="pt-2 relative mx-auto text-gray-600" autocomplete="off">
+<form  class="pt-2 relative mx-auto text-gray-600" autocomplete="off">
     <input wire:model.live="search" class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
            type="search" name="search" placeholder="Search">
 
-    <button type="button" wire:click="resetSearch" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 
+    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 
             rounded absolute right-0 top-0 mt-2">
-        Reset
+        Buscar
     </button>
 
     @if ($search != "")
