8e1dda15cbbfe710383c75385e1a0a56a079f39b
Migue81 <81migue81@gmail.com>
2024-09-18 12:56:51 +0200

    listar courses

--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -1,8 +1,29 @@
 <div>
+
     <div class="bg-gray-200 py-4 mb-16">  
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex"> 
-            <div class="grid grid-cols-4 gap-x-4"> <!-- Ajuste de columnas para incluir el nuevo filtro -->
+            <div class="grid grid-cols-5 gap-x-4">
                 
+                <!-- Filtro por Precio -->
+                <div class="col-span-2">
+                    <form wire:submit.prevent="filterPrices" class="flex items-center justify-end space-x-4">
+                        <div>
+                            <select wire:model="selectedCategories" id="selectedPrices" name="selectedPrices" class="h-10 border-gray-300 rounded-lg">
+                                <option class="py-2" value="">Seleccione un Precio</option>
+                                @foreach($prices as $price)
+                                    <option value="{{$price->id}}">{{$price->value}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <i class="fas fa-dollar-sign text-xs mr-2"></i>  
+                            Filtrar por Precios
+                        </button>
+                    </form>
+                </div>
+
+
                 <!-- Filtro por Categoría -->
                 <div class="col-span-2">
                     <form wire:submit.prevent="filterCategories" class="flex items-center justify-end space-x-4">
@@ -41,41 +62,24 @@
                     </form>
                 </div>
 
-                <!-- Filtro por Precio -->
-                <div class="col-span-2">
-                    <form wire:submit.prevent="filterPrices" class="flex items-center justify-end space-x-4">
-                        <div>
-                            <select wire:model="selectedPrices" id="selectedPrices" name="selectedPrices" class="h-10 border-gray-300 rounded-lg">
-                                <option class="py-2" value="">Seleccione el Precio</option>
-                                @foreach($prices as $price)
-                                    <option value="{{$price->id}}">{{$price->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <i class="fas fa-dollar-sign text-xs mr-2"></i>  <!-- Ícono mejorado -->
-                            Filtrar por Precios
-                        </button>
-                    </form>
-                </div>
-
             </div>
         </div>
     </div>
 
-    <!-- Mostrar Cursos Paginados -->
+    <!-- Mostrar la colección total paginada, y las filtradas no paginadas -->
     @if($courses->isNotEmpty())
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8">
             @foreach ($courses as $course)
                 <x-instructor.course-card :course="$course" />
             @endforeach
         </div>
-        @unless($isFiltered) <!-- Mostrar paginación solo si no está filtrado -->
+
+        @unless($isFiltered) <!--Mostrar paginación solo si la colección, no está filtrada-->
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4 mb-8">
                 {{ $courses->links('pagination::tailwind') }}
             </div>
         @endunless
+
     @else
         <div class="bg-gray-100 rounded-lg p-4 text-center w-full">
             <p class="text-gray-500 font-bold block">{{ $mensaje }}</p>
