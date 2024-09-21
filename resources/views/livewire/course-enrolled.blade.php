970a9c11cf6b8cb55cfb89919be14c2290e9f37c
Migue81 <81migue81@gmail.com>
2024-09-21 12:29:40 +0200

    carrito3

--- resources/views/livewire/course-enrolled.blade.php
+++ resources/views/livewire/course-enrolled.blade.php
@@ -36,38 +36,41 @@
 
         <div class="container mx-auto py-6">
             <h1 class="text-2xl font-semibold mb-4">Contenido del Carrito de Prueba</h1>
-    
+        
             @if($cartContent->isEmpty())
                 <p>No hay elementos en el carrito.</p>
             @else
-                <table class="min-w-full bg-white">
-                    <thead>
-                        <tr>
-                            <th class="w-1/4 px-6 py-3 text-left">Nombre</th>
-                            <th class="w-1/4 px-6 py-3 text-left">Cantidad</th>
-                            <th class="w-1/4 px-6 py-3 text-left">Precio</th>
-                            <th class="w-1/4 px-6 py-3 text-left">Opciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($cartContent as $item)
-                            <tr>
-                                <td class="border px-6 py-3">{{ $item->name }}</td>
-                                <td class="border px-6 py-3">{{ $item->qty }}</td>
-                                <td class="border px-6 py-3">{{ $item->price }} €</td>
-                                <td class="border px-6 py-3">
+                <div class="bg-white p-6 rounded-lg shadow-lg">
+                    @foreach($cartContent as $item)
+                        <div class="mb-6 border-b border-gray-200 pb-3">
+                            <div class="flex mb-2">
+                                <div class="w-1/4 font-bold">Nombre:</div>
+                                <div class="w-3/4">{{ $item->name }}</div>
+                            </div>
+                            <div class="flex mb-2">
+                                <div class="w-1/4 font-bold">Cantidad:</div>
+                                <div class="w-3/4">{{ $item->qty }}</div>
+                            </div>
+                            <div class="flex mb-2">
+                                <div class="w-1/4 font-bold">Precio:</div>
+                                <div class="w-3/4">{{ $item->price }} €</div>
+                            </div>
+                            <div class="flex">
+                                <div class="w-1/4 font-bold">Opciones:</div>
+                                <div class="w-3/4">
                                     <ul>
                                         @foreach($item->options as $optionKey => $optionValue)
                                             <li><strong>{{ ucfirst($optionKey) }}</strong>: {{ $optionValue }}</li>
                                         @endforeach
                                     </ul>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             @endif
         </div>
+        
 </div>
 
 
