aa650868dbc10e3268b09600bd49447981a13b0a
Migue81 <81migue81@gmail.com>
2024-09-21 19:06:07 +0200

    carrito12

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//Paquete carrito de compra traido de github
+use CodersFree\Shoppingcart\Facades\Cart;  
 
 class CartController extends Controller
 {
     public function index(){
-        return view('cart.index');
+        // return view('cart.index');
+                // Obtén el contenido del carrito
+                $cartContent = Cart::instance('shopping')->content();
+
+                // Retorna la vista con el contenido del carrito
+                return view('cart.index', compact('cartContent'));
     }
 }
