8892e12bab57ce0c2c039ff66a0b0e596fad2843
Migue81 <81migue81@gmail.com>
2024-09-26 19:45:40 +0200

    barra de navegación3

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -237,7 +237,7 @@
         @auth
             <div class="pt-4 pb-1 border-t border-gray-200">
                 
-                    <div class="flex items-center px-4">
+                    {{-- <div class="flex items-center px-4">
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                             <div class="shrink-0 me-3">
                                 <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
@@ -247,7 +247,7 @@
                         <div>
                             <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                             <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                        </div>
+                        </div> --}}
                     </div>
                 
 
