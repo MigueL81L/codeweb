9013ea2e490fa1937f8b533213be0dcd2da2c744
Migue81 <81migue81@gmail.com>
2024-09-30 16:50:37 +0200

    probando

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -237,8 +237,11 @@
                 </div>
 
                 <!-- User Icon Button and Cart Icon -->
+
                 <div class="flex flex-col items-center">
-                    <div class="relative" x-data="{ dropdownOpen: false }">
+
+                    <!--User Icon/Foto-->
+                    {{-- <div class="relative" x-data="{ dropdownOpen: false }">
                         <button @click="dropdownOpen = !dropdownOpen" class="flex items-center justify-center h-8 w-8 rounded-full bg-gray-500 text-white focus:outline-none">
                             <span class="text-sm font-medium leading-none">{{ strtoupper(substr(Auth::user()->name, 0, 1)) }}</span>
                         </button>
@@ -257,7 +260,79 @@
                                 </button>
                             </form>
                         </div>
+                    </div> --}}
+
+                    <div class="ms-4 relative">
+                        @auth
+                                <x-dropdown align="right" width="48">
+                                    <x-slot name="trigger">
+                                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                            </button>
+                                        @else
+                                            <span class="inline-flex rounded-md">
+                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                                    {{ Auth::user()->name }}
+    
+                                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        @endif
+                                    </x-slot>
+    
+                                    <x-slot name="content">
+                                        <!-- Account Management -->
+                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                            {{ __('Manage Account') }}
+                                        </div>
+    
+                                        <x-dropdown-link href="{{ route('profile.show') }}">
+                                            {{ __('Profile') }}
+                                        </x-dropdown-link>
+    
+    
+                                        <div class="border-t border-gray-200"></div>
+    
+                                        <!-- Authentication -->
+                                        <form method="POST" action="{{ route('logout') }}" x-data>
+                                            @csrf
+    
+                                            <x-dropdown-link href="{{ route('logout') }}"
+                                                    @click.prevent="$root.submit();">
+                                                {{ __('Log Out') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                    </x-slot>
+                                </x-dropdown>
+    
+                            @else
+    
+                                <x-dropdown align="right" width="48">
+                                    <x-slot name="trigger">
+                                        <button class="text-gray-600">
+                                            <i class="fa-regular fa-circle-user text-2xl"></i>
+                                        </button>
+                                    </x-slot>
+    
+                                    <x-slot name="content">
+                                        <!--Las rutas login y register se crearon por defecto con jetstream-->
+                                        <x-dropdown-link href="{{ route('login') }}">
+                                            Iniciar Sesión
+                                        </x-dropdown-link>
+                                        <x-dropdown-link href="{{ route('register') }}">
+                                            Registrarse
+                                        </x-dropdown-link>
+    
+                                    </x-slot>
+                                </x-dropdown>
+                        @endauth
+    
                     </div>
+
+
 
                     <!-- Cart Icon -->
                     <div class="relative mt-2"
