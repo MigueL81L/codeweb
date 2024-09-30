f584f2ad73174fe07097e806c4a4860e1ac121e8
Migue81 <81migue81@gmail.com>
2024-09-30 10:09:57 +0200

    r3

--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="bg-gray-200 py-4 mb-16">  
         {{-- <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">  --}}
-            <div class="grid grid-cols-1 md:grid-cols-3 md:gap-x-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 md:gap-x-4 md:py-4">
                 <!-- Filtro por Precio -->
                 <div class="col-span-1">
                     <form wire:submit.prevent="filterPrices" class="flex items-center justify-center space-x-4">
