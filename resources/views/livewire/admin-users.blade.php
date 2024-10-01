8083fbe5ffca85de718d972af9d3d1b8cbddf95c
Migue81 <81migue81@gmail.com>
2024-10-01 12:30:24 +0200

    rusers3

--- resources/views/livewire/admin-users.blade.php
+++ resources/views/livewire/admin-users.blade.php
@@ -79,7 +79,9 @@
     </div>
 </div> --}}
 
-<div>
+
+
+{{-- <div>
     <div class="card-body">
 
         @if(session('info'))
@@ -158,7 +160,87 @@
             </form>
         </div>
     </div>
+</div> --}}
+
+<div>
+    <div class="card-body">
+        @if(session('info'))
+            <div class="bg-blue-500 text-white px-4 py-2 w-full shadow-md mb-2">
+                <strong>Éxito! </strong>{{ session('info') }}
+            </div>
+        @endif
+
+        @if (session('warning'))
+            <div class="bg-yellow-500 text-white px-4 py-2 w-full shadow-md mb-2">
+                <strong>Advertencia! </strong>{{ session('warning') }}
+            </div>
+        @endif
+
+        <div class="table-responsive overflow-x-auto">
+            <div class="flex flex-col sm:flex-row justify-between items-center mb-4 space-y-4 sm:space-y-0">
+                <div class="flex flex-col sm:flex-row items-center space-y-4 sm:space-x-4">
+                    <form wire:submit.prevent="filterUsers" class="flex items-center relative text-gray-600 space-x-4">
+                        <select wire:model="selectedRoles" class="h-10 border-gray-300 rounded-lg w-full sm:w-auto">
+                            <option value="">Seleccione un Rol</option>
+                            @foreach($roles as $role)
+                                <option value="{{ $role->id }}">{{ $role->name }}</option>
+                            @endforeach
+                        </select>
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto">
+                            Filtrar Usuarios por Rol
+                        </button>
+                    </form>
+                </div>
+
+                <a href="{{ route('admin.users.create') }}" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full sm:w-auto text-center">
+                    Crear Nuevo Usuario
+                </a>
+            </div>
+
+            <form class="pt-2 relative mx-auto text-gray-600 w-full" autocomplete="off">
+                <input wire:model.live="search" class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" type="search" name="search" placeholder="Escribe un nombre o un email...">
+
+                <table class="w-full border-collapse mt-4">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2">Nombre</th>
+                            <th class="px-4 py-2">Email</th>
+                            <th class="px-4 py-2 text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($paginatedUsers as $user)
+                            <tr class="{{ $loop->even ? 'bg-gray-100' : 'bg-white' }}">
+                                <td class="border px-4 py-2 text-center">{{ $user->name }}</td>
+                                <td class="border px-4 py-2 text-center">{{ $user->email }}</td>
+                                <td class="border px-4 py-2 text-center">
+                                    <!-- Acciones flexibles -->
+                                    <div class="flex flex-col sm:flex-row items-center justify-center space-y-2 sm:space-y-0 sm:space-x-2">
+                                        <a class="btn btn-secondary" href="{{ route('admin.users.edit', $user) }}">Editar</a>
+                                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST" class="inline-block" onsubmit="return confirm('¿Estás seguro de que deseas eliminar este usuario?')">
+                                            @method('delete')
+                                            @csrf
+                                            <button class="btn btn-danger" type="submit">Eliminar</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="3" class="border px-4 py-2 text-center">No hay usuarios que coincidan con su búsqueda</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+
+                <div class="mt-4">
+                    {{ $paginatedUsers->links() }}
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+
 
 
 
