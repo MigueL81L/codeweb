c28777fdd5d0138282fa21fb222f665dfbafac4d
Migue81 <81migue81@gmail.com>
2024-10-02 16:42:55 +0200

    sidebar8

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -38,8 +38,8 @@
         @include('layouts.includes.admin.sidebar')
 
         <button @click="toggleSidebar()" class="m-4 p-2 bg-gray-800 text-white sm:hidden">
-            <i :class="open ? 'fa-solid fa-gauge' : 'fas fa-bars'"></i>
-            <span class="ml-2">Barra de Administración</span>
+            <i :class="open ? 'fas fa-bars' : 'fa-solid fa-gauge'"></i>
+            <span class="ml-2">Barra de Administración</span> fas fa-bars
         </button>
 
         <div class="p-4 sm:ml-64">
