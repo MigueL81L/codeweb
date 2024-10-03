d870609d5884d7bcaaddd17c5f16f3e378c19c59
Migue81 <81migue81@gmail.com>
2024-10-03 17:16:03 +0200

    sidebar28

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -43,9 +43,9 @@
             <span class="ml-2">Barra de Administración</span>
         </button>
 
-        <div class="p-4 sm:ml-64 mt-18">
+        <div class="p-4 sm:ml-64 mt-20">
             <!--Aplicación de las migas de pan-->
-            <div class="mt-2">
+            <div>
                 @include('layouts.includes.admin.breadcrumb')
                 <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
                     {{$slot}}
