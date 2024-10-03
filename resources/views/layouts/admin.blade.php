fbd1c6a4f4f692dd2b26f6dc4ff53da2534aa374
Migue81 <81migue81@gmail.com>
2024-10-03 17:06:50 +0200

    sidebar25

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -45,7 +45,7 @@
 
         <div class="p-4 sm:ml-64">
             <!--Aplicación de las migas de pan-->
-            <div class="mt-7">
+            <div class="mt-14">
                 @include('layouts.includes.admin.breadcrumb')
                 <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
                     {{$slot}}
