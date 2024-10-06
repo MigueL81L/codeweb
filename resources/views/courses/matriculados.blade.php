2aee9d02c8199c3eb383a4bf6f976265ca7c8b4c
Migue81 <81migue81@gmail.com>
2024-10-06 12:59:20 +0200

    portada5

--- resources/views/courses/matriculados.blade.php
+++ resources/views/courses/matriculados.blade.php
@@ -1,8 +1,8 @@
 
 <x-app-layout>
     <!--Cabecera, Buscador-->
-    {{-- <section style="background-image: url({{ asset('img/cursos/matriculados.jpg') }})" class="bg-cover"> --}}
-    <section style="background-image: url({{ asset('img/logo/Logo64.png') }})" class="bg-cover">
+    <section style="background-image: url({{ asset('img/cursos/matriculados.jpg') }})" class="bg-cover">
+    {{-- <section style="background-image: url({{ asset('img/logo/Logo64.png') }})" class="bg-cover"> --}}
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
                 <h1 class="text-white font-bold text-4xl">Tus cursos en los que has matriculado</h1>
