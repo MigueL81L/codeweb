0b4d0e47e948df9b04adb3ed3d94d5f687169234
Migue81 <81migue81@gmail.com>
2024-10-06 13:32:34 +0200

    portadas9

--- resources/views/courses/matriculados.blade.php
+++ resources/views/courses/matriculados.blade.php
@@ -1,7 +1,7 @@
 
-{{-- <x-app-layout>
+<x-app-layout>
     <!--Cabecera, Buscador-->
-    <section style="background-image: url({{ asset('img/cursos/matriculados.jpg') }})" class="bg-cover">
+    <section style="background-image: url({{ asset('img/cursos/matriculados.jpg') }})" class="bg-cover h-screen">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
                 <h1 class="text-white font-bold text-4xl">Tus cursos en los que has matriculado</h1>
@@ -14,17 +14,6 @@
 
     <!--Llamo al componente livewire CourseIndex previamente creado-->
     @livewire('course-matriculados');  
-</x-app-layout> --}}
-
-<x-app-layout>
-    <div class="text-center py-36">
-        <h1 class="text-white font-bold text-4xl">Tus cursos en los que has matriculado</h1>
-        <p class="text-white text-lg mt-2 mb-4">Si estás buscando potenciar tus conocimientos de 
-            programación, has llegado al lugar adecuado. Estos son los cursos, en los que te has matriculado.
-        </p>
-        <img src="{{ asset('img/cursos/matriculados.png') }}" alt="Cursos Matriculados" class="max-w-full h-auto">
-    </div>
-
-    <!--Llamo al componente livewire CourseIndex previamente creado-->
-    @livewire('course-matriculados');  
 </x-app-layout>
+
+
