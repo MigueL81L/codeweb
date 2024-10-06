9d0ce4205294e4ff7798fa275475d795b71c0b81
Migue81 <81migue81@gmail.com>
2024-10-06 13:19:37 +0200

    portadas8

--- resources/views/courses/matriculados.blade.php
+++ resources/views/courses/matriculados.blade.php
@@ -1,9 +1,7 @@
 
-<x-app-layout>
+{{-- <x-app-layout>
     <!--Cabecera, Buscador-->
-    {{-- <section style="background-image: url({{ asset('img/cursos/matriculados.jpg') }})" class="bg-cover"> --}}
-    {{-- <section style="background-image: url({{ asset('img/cursos/matriculados.png') }})" class="bg-cover"> --}}
-        <section style="background-image: url({{ asset('img/logo/matriculados.png') }})" class="bg-cover">
+    <section style="background-image: url({{ asset('img/cursos/matriculados.jpg') }})" class="bg-cover">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
                 <h1 class="text-white font-bold text-4xl">Tus cursos en los que has matriculado</h1>
@@ -13,6 +11,19 @@
             </div>
         </div>
     </section>
+
+    <!--Llamo al componente livewire CourseIndex previamente creado-->
+    @livewire('course-matriculados');  
+</x-app-layout> --}}
+
+<x-app-layout>
+    <div class="text-center py-36">
+        <h1 class="text-white font-bold text-4xl">Tus cursos en los que has matriculado</h1>
+        <p class="text-white text-lg mt-2 mb-4">Si estás buscando potenciar tus conocimientos de 
+            programación, has llegado al lugar adecuado. Estos son los cursos, en los que te has matriculado.
+        </p>
+        <img src="{{ asset('img/cursos/matriculados.png') }}" alt="Cursos Matriculados" class="max-w-full h-auto">
+    </div>
 
     <!--Llamo al componente livewire CourseIndex previamente creado-->
     @livewire('course-matriculados');  
