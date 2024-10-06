d73c64e9c375952cb6167c5b7a72e8cf7a382aba
Migue81 <81migue81@gmail.com>
2024-10-06 18:06:33 +0200

    portadas14

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -53,6 +53,8 @@
                 <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
                     {{$slot}}
                 </div>
+
+                @include('layouts.includes.app.footer')
             </div>
         </div>
 
