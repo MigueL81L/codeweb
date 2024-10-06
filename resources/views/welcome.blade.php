eba35b3eab24513f688dbc42678234334243c4c2
Migue81 <81migue81@gmail.com>
2024-10-06 11:14:16 +0200

    fotos portada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <!--El método asset hace referencia a mi carpeta public-->
     <!--Cabecera, Buscador-->
-    <section style="background-image: url({{ asset('img/home/pexelsHome.jpg') }})" class="bg-cover">
+    {{-- <section style="background-image: url({{ asset('img/home/pexelsHome.jpg') }})" class="bg-cover"> --}}
+        <section style="background-image: url('https://codeweb.pw/img/home/pexelsHome.jpg')" class="bg-cover">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
                 <h1 class="text-white font-bold text-4xl">Domina la tecnología Web</h1>
