4b3031a252385999aab5624db46bcae723fba0ee
Migue81 <81migue81@gmail.com>
2024-10-07 20:21:08 +0200

    admin14

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,12 +4,37 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        {{-- <meta name="viewport" content="width=device-width, initial-scale=1">  --}}
-        <meta name="viewport" content="width=375, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+        <style>
+            /* Asegúrate de que la aplicación ocupe toda la ventana */
+            body {
+              overflow: hidden; /* Oculta cualquier desbordamiento */
+            }
+            
+            #app {
+              width: 100vw;  /* Abarca todo el viewport */
+              height: 100vh; /* Mantiene la altura igual que el viewport */
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+          </style>
+
+
+
+
+
+
+
+
+
 
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('public/Logo64.png') }}" type="image/png">
@@ -35,46 +60,67 @@
         class="sm:overflow-auto"
         :class="{'overflow-hidden': open}">
 
-        {{-- Para que las páginas de admin, home, ..etc, usen el mismo navigation-menu --}}
-        {{-- @livewire('navigation-menu')    --}}
-        @include('navigation-menu')
+        <div id="app">
+                
+            
 
-        <div class="flex border-b border-gray-200">
-            <!-- Columna Izquierda - Sidebar -->
-            <div class="flex-none">
-                @include('layouts.includes.admin.sidebar')
-            </div>
-        
-            <!-- Columna Derecha - Contenido Principal -->
-            <div class="flex-grow p-4 border-l border-gray-200 w-full mt-8">
+            {{-- Para que las páginas de admin, home, ..etc, usen el mismo navigation-menu --}}
+            {{-- @livewire('navigation-menu')    --}}
+            @include('navigation-menu')
 
-                <!-- Botón para mostrar/ocultar sidebar (visible solo en móvil) -->
-                <button @click="toggleSidebar()" class="m-4 p-2 bg-gray-800 text-white sm:hidden">
-                    <i :class="open ? 'fas fa-bars' : 'fa-solid fa-gauge'"></i>
-                    <span class="ml-2">Barra de Administración</span>
-                </button>
-        
-                <!-- Migas de pan -->
-                @include('layouts.includes.admin.breadcrumb')
-        
-                <!-- Contenido principal -->
-                <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mb-4">
-                    {{$slot}}
+            <div class="flex border-b border-gray-200">
+                <!-- Columna Izquierda - Sidebar -->
+                <div class="flex-none">
+                    @include('layouts.includes.admin.sidebar')
+                </div>
+            
+                <!-- Columna Derecha - Contenido Principal -->
+                <div class="flex-grow p-4 border-l border-gray-200 w-full mt-8">
+
+                    <!-- Botón para mostrar/ocultar sidebar (visible solo en móvil) -->
+                    <button @click="toggleSidebar()" class="m-4 p-2 bg-gray-800 text-white sm:hidden">
+                        <i :class="open ? 'fas fa-bars' : 'fa-solid fa-gauge'"></i>
+                        <span class="ml-2">Barra de Administración</span>
+                    </button>
+            
+                    <!-- Migas de pan -->
+                    @include('layouts.includes.admin.breadcrumb')
+            
+                    <!-- Contenido principal -->
+                    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mb-4">
+                        {{$slot}}
+                    </div>
                 </div>
             </div>
-        </div>
+            
         
-    
-        @include('layouts.includes.app.footer')
+            @include('layouts.includes.app.footer')
 
 
-        {{-- <div x-cloak x-show="open" x-on:click="toggleSidebar()" class="bg-gray-900 bg-opacity-50 fixed inset-0 z-30 sm:hidden pointer-events-auto"></div> --}}
+            {{-- <div x-cloak x-show="open" x-on:click="toggleSidebar()" class="bg-gray-900 bg-opacity-50 fixed inset-0 z-30 sm:hidden pointer-events-auto"></div> --}}
 
+        </div>
 
+            @stack('modals')
 
-        @stack('modals')
+            @livewireScripts
 
-        @livewireScripts
+            <script>
+                // Función para verificar la orientación
+                function checkOrientation() {
+                  if (window.innerHeight < window.innerWidth) {
+                    // Si está en apaisado, prevenir ajustes del layout
+                    alert('Por favor, utiliza la pantalla en vertical para una mejor experiencia.'); // Esto es solo indicativo, lo puedes quitar.
+                    window.scrollTo(0, 0); // Asegúrate de que no se mueva la vista.
+                  }
+                }
+          
+                // Registrar el evento de cambio de orientación
+                window.addEventListener('resize', checkOrientation);
+                window.addEventListener('orientationchange', checkOrientation); // Ajusta los cambios de orientación
+                window.addEventListener('load', checkOrientation); // Verifica la orientación al cargar la página
+              </script>
+            
 
     </body>
     <script>
