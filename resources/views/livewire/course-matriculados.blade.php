80e17cccfb3e19dd38f88831fd91e1395ea9dcec
Migue81 <81migue81@gmail.com>
2024-10-09 13:29:04 +0200

    filtros13

--- resources/views/livewire/course-matriculados.blade.php
+++ resources/views/livewire/course-matriculados.blade.php
@@ -6,7 +6,7 @@
                 <!-- Filtro por Precio -->
                 <div class="col-span-1">
                     <form wire:submit.prevent="filterPrices" class="flex items-center justify-between  lg:justify-center">
-                        <div class="flex-grow mr-2">
+                        <div class="flex-grow ">
                             <select wire:model="selectedPrices" id="selectedPrices" name="selectedPrices" class="h-10 border-gray-300 justify-start lg:justify-center mb-2 lg:mb-0 ml-2 lg:ml-0 rounded-lg">
                                 <option class="py-2" value="">Seleccione un Precio</option>
                                 @foreach($prices as $price)
