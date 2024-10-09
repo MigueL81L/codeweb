3323ddece1e6ff92e55770241b68f0c6e98822a0
Migue81 <81migue81@gmail.com>
2024-10-09 11:59:07 +0200

    filtros

--- resources/views/livewire/course-matriculados.blade.php
+++ resources/views/livewire/course-matriculados.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-gray-200 py-4 mb-16">  
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">  
 
-            <div class="grid grid-cols-1 sm:grid-cols-3 sm:gap-x-4 sm:py-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 sm:gap-x-4 sm:py-4"> 
                 <!-- Filtro por Precio -->
                 <div class="col-span-1">
                     <form wire:submit.prevent="filterPrices" class="flex items-center justify-between  sm:justify-center sm:space-x-4">
