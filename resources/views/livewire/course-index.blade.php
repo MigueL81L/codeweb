4ef00663871343e3fe3ad6c452144e6b899a8fec
Migue81 <81migue81@gmail.com>
2024-10-09 17:30:48 +0200

    filtros22

--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -4,9 +4,9 @@
             <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-4 lg:py-4">
                 <!-- Filtro por Precio -->
                 <div class="col-span-1">
-                    <form wire:submit.prevent="filterPrices" class="flex items-center justify-between  sm:justify-center sm:space-x-4">
+                    <form wire:submit.prevent="filterPrices" class="flex items-center justify-around  sm:justify-between sm:space-x-4">
                         <div class="flex-grow mr-2">
-                            <select wire:model="selectedPrices" id="selectedPrices" name="selectedPrices" class="h-10 border-gray-300 justify-start sm:justify-center mb-2 sm:mb-0 ml-2 sm:ml-0 rounded-lg">
+                            <select wire:model="selectedPrices" id="selectedPrices" name="selectedPrices" class="h-10 border-gray-300 justify-start lg:justify-center mb-2 lg:mb-0 ml-2 lg:ml-0 rounded-lg">
                                 <option class="py-2" value="">Seleccione un Precio</option>
                                 @foreach($prices as $price)
                                     <option value="{{$price->id}}">{{$price->value}}</option>
@@ -14,7 +14,7 @@
                             </select>
                         </div>
                         
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded justify-end sm:justify-center mb-2 sm:mb-0 mr-2 sm:mr-0">
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded justify-end lg:justify-center mb-2 lg:mb-0 mr-2 lg:mr-0">
                             <i class="fas fa-euro-sign text-xs mr-2"></i>  
                             Filtrar
                         </button>
