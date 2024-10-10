682d6986674b4debfbbc5c30cc0876828ca81d1e
Migue81 <81migue81@gmail.com>
2024-10-10 19:38:59 +0200

    register

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('register') }}" class="flex items-center space-x-2">
-    <img src="{{ asset('img/logo/Logo64.png') }}" alt="Logo" style="width: 64px; height: 64px;">
-    <span class="text-xl font-semibold text-gray-800 font-serif">CodeWeb</span>
+    <img src="{{ asset('public/img/logo/Logo64.png') }}" alt="Logo" style="width: 64px; height: 64px;">
+    <span class="text-xl font-semibold text-gray-800 font-roboto">CodeWeb</span>
 </a>
 
