2d0c72e4646a6dd9d867389bb4682acd05e193cc
Migue81 <81migue81@gmail.com>
2024-10-10 18:41:28 +0200

    build2

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('home') }}" class="flex items-center space-x-2 hover:text-blue-600">
-  <img src="{{ asset('img/logo/Logo64.png') }}" alt="Logo" {{ $attributes }} style="width: 48px; height: 48px;">
+  <img src="{{ asset('public/img/logo/Logo64.png') }}" alt="Logo" {{ $attributes }} style="width: 48px; height: 48px;">
   <span class="text-xl font-semibold text-gray-800 font-serif">CodeWeb</span>
 </a>
 
