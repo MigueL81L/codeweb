135da3d839b5a435a0ec9d43e747aa4510f5709a
Migue81 <81migue81@gmail.com>
2024-10-17 18:41:13 +0200

    show4

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -156,7 +156,7 @@
                 </div>
             </section>
 
-            <aside class="hidden lg:block lg:mb-4 lg:bg-white lg:shadow-lg lg:rounded lg:overflow-hidden">
+            <aside class="hidden lg:block mb-4 bg-white shadow-lg rounded overflow-hidden">
                 <h1 class="font-bold text-3xl mb-4">Cursos Similares</h1>
                 @if ($similares->count()!=0)
                     @foreach ($similares as $similar)
