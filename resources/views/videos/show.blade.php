e41b2ff8af89ded07e4f7455f219f5af15ff0840
Migue81 <81migue81@gmail.com>
2024-10-21 21:34:55 +0200

    status50

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -2,16 +2,16 @@
 <html>
 <head>
     <title>Video</title> 
-    {{-- <style>
+    <style>
         
-        .embed-responsive {
+        .embed-responsive1 {
             position: relative;
             overflow: hidden;
             padding-top: 56.25%; 
         }
 
-        .embed-responsive video,
-        .embed-responsive iframe {
+        .embed-responsive1 video,
+        .embed-responsive1 iframe {
             position: absolute;
             top: 0;
             left: 0;
@@ -19,13 +19,10 @@
             height: 100%;
             border: 0;
         }
-    </style> --}}
-
-    <link rel="stylesheet" href="{{ asset('build/assets/app-KEcVs8Hg.css') }}">
-    <script src="{{ asset('build/assets/app-49Ykkm2g.js') }}" defer></script>
+    </style>
 </head>
     <body>
-            <div class="embed-responsive w-full"> <!-- Asegura que ocupa todo el ancho -->
+            <div class="embed-responsive1 w-full"> <!-- Asegura que ocupa todo el ancho -->
                 <video class="w-full h-auto" controls>
                     <source src="{{ Storage::url('app/public/' . $lesson->video_path) }}" type="{{ $lesson->getVideoType($lesson->video_original_name) }}">    
                     Your browser does not support the video tag.
