d5ecded0a1a6e1462a5af2eef5e6f376c97a1424
Migue81 <81migue81@gmail.com>
2024-10-21 20:57:51 +0200

    status45

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -3,11 +3,16 @@
 <head>
     <title>Video</title> 
 </head>
-<body>
-    <video width="560" height="315" controls>
-        {{-- <source src="{{ Storage::url($lesson->video_path) }}" type="{{ $lesson->getVideoType($lesson->video_original_name) }}"> --}}
-        <source src="{{ Storage::url('app/public/' . $lesson->video_path) }}" type="{{ $lesson->getVideoType($lesson->video_original_name) }}">    
-        Your browser does not support the video tag.
-    </video>
-</body>
+    <body>
+            {{-- <video width="560" height="315" controls>
+                <source src="{{ Storage::url('app/public/' . $lesson->video_path) }}" type="{{ $lesson->getVideoType($lesson->video_original_name) }}">    
+                Your browser does not support the video tag.
+            </video> --}}
+            <div class=" w-full embed-responsive"> <!-- Mantiene la relación de aspecto -->
+                <video class="w-full h-auto" controls>
+                    <source src="{{ Storage::url('app/public/' . $lesson->video_path) }}" type="{{ $lesson->getVideoType($lesson->video_original_name )}}">    
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+    </body>
 </html>
