8a227065409714f1fe2c954345ae15a6224e1fb4
Migue81 <81migue81@gmail.com>
2024-10-21 10:31:15 +0200

    cart15

--- resources/views/livewire/cart-manager.blade.php
+++ resources/views/livewire/cart-manager.blade.php
@@ -11,10 +11,9 @@
                     
                     <div class="ml-4 flex-1">
                         <h2 class="text-lg font-bold">{{ $item->name }}</h2>
-                        <p class="text-sm text-gray-500">Nombre: {{ $item->options['title'] }}</p>
+                        <p class="text-sm text-gray-500">Nombre: {{ $item->name }}</p>
                         <p class="text-sm text-gray-500">Profesor: {{ $item->options['teacher'] }}</p>
-                        <p class="text-sm text-gray-500">Categoría: {{ $item->category->name}}</p>
-                        <p class="text-sm text-gray-500">Precio: {{ $item->price->name .  ' €' }}</p>
+                        <p class="text-sm text-gray-500">Precio: {{ $item->price .  ' €' }}</p>
 
                         <div class="mt-4">
                             {{-- Botón para eliminar el curso de la cesta --}}
