b7a1deee4e79a17f335bca3e75bb82dc93d71626
Migue81 <81migue81@gmail.com>
2024-10-22 16:40:13 +0200

    footer

--- resources/views/layouts/includes/app/footer.blade.php
+++ resources/views/layouts/includes/app/footer.blade.php
@@ -3,28 +3,28 @@
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8"> 
         <div class="flex flex-col items-center text-center sm:flex-row sm:text-left sm:items-start md:justify-between"> 
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-16 sm:gap-12 flex-grow w-full sm:w-auto">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4 sm:gap-12 flex-grow w-full sm:w-auto">
                 <div class="mb-4 sm:mb-6 flex justify-center w-full sm:w-auto">
                     <x-application-mark class="h-8 mr-3" />
                 </div>
                 <div class="flex flex-col sm:items-start sm:text-left md:items-center md:text-center">
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Nosotros</h2>
+                    <h2 class="mb-6 font-semibold text-gray-900 uppercase dark:text-white">Nosotros</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
+                        <li class="mb-2">
                             <a href="{{ route('who-we-are') }}" class="hover:cursor-pointer hover:text-blue-600 no-underline">Quienes Somos</a>
                         </li>
-                        <li class="mb-4">
+                        <li class="mb-2">
                             <a href="{{ route('contact') }}" class="hover:cursor-pointer hover:text-blue-600 no-underline">Contáctanos</a>
                         </li>
                     </ul>
                 </div>
                 <div class="flex flex-col sm:items-start sm:text-left md:items-center md:text-center">
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Compañía</h2>
+                    <h2 class="mb-6 font-semibold text-gray-900 uppercase dark:text-white">Compañía</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
+                        <li class="mb-2">
                             <a href="{{ route('privacy-policy') }}" class="hover:cursor-pointer hover:text-blue-600 no-underline">Política de Privacidad</a>
                         </li>
-                        <li class="mb-4">
+                        <li class="mb-4¡2">
                             <a href="{{ route('terms-and-conditions') }}" class="hover:cursor-pointer hover:text-blue-600 no-underline">Términos y Condiciones</a>
                         </li>
                         <li>
