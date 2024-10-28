2e523fb9195d1ab5c2734ab7fd53e4faae535fff
Migue81 <81migue81@gmail.com>
2024-10-28 18:44:17 +0100

    register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CartController;
@@ -8,65 +7,61 @@ use App\Http\Controllers\Instructor\CourseController;
 use App\Http\Controllers\Admin\UserController;
 use App\Models\Lesson;
 use App\Models\Course;
-use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-//Importo un componente livewire
-use App\Livewire\CourseStatus;
-use Livewire\Livewire;
-use App\Http\Controllers\Instructor\InstructorController;
 
 require __DIR__.'/instructor.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/externos.php';
 
 
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
 
-// Ruta para mostrar el video de una lección específica de un curso para el que el usuario está matriculado
-// Route::get('instructor/video/{id}', [\App\Http\Controllers\Instructor\VideoLessonController::class, 'show'])
-//     ->name('instructor.video.show');
-
-Route::get('videos/{id}', [\App\Http\Controllers\Instructor\VideoLessonController::class, 'show'])
-    ->name('videos.show');
+// Route::get('videos/{id}', [\App\Http\Controllers\Instructor\VideoLessonController::class, 'show'])
+//     ->name('videos.show');
     
 
-Route::get('/', HomeController::class)->name('home');
+// Route::get('/', HomeController::class)->name('home');
+// Route::get('cursos', [CourseController::class, 'list'])->name('courses.index');
+// Route::get('cursos/{course}', [CourseController::class, 'show'])->name('courses.show');
+// Route::get('courses', [CourseController::class, 'matriculados'])->name('courses.matriculados');
 
-// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function(){
-//     return view('dashboard');
-// })->name('dashboard');
+// });
 
-//En el ejemplo es 'index', no 'list'
-Route::get('cursos', [CourseController::class, 'list'])->name('courses.index');
 
-Route::get('cursos/{course}', [CourseController::class, 'show'])->name('courses.show');
+// Route::get('cart', [CartController::class, 'index'])->name('cart.index');
+// Route::post('courses/{course}/enrolled', [CourseController::class, 'enrolled'])
+// ->middleware('auth')->name('course.enrolled');
+// Route::get('course-status/{course}', function (Course $course) {
+//     return view('courses.status', ['course' => $course]); 
+// })->name('courses.status')->middleware('auth');
 
-//Ruta para mostrar al usuario, solo los cursos en los que está matriculado
-Route::get('courses', [CourseController::class, 'matriculados'])->name('courses.matriculados');
 
+
+
+// Redirigir a la página de registro si el usuario no está autenticado
+Route::get('/', function () {
+    return Auth::check() ? redirect()->route('home') : redirect()->route('register');
 });
 
+Route::middleware(['auth'])->group(function () {
+    // Ruta para mostrar el video de una lección específica de un curso para el que el usuario está matriculado
+    Route::get('videos/{id}', [\App\Http\Controllers\Instructor\VideoLessonController::class, 'show'])
+        ->name('videos.show');
+    
+    Route::get('cursos', [CourseController::class, 'list'])->name('courses.index');
+    Route::get('cursos/{course}', [CourseController::class, 'show'])->name('courses.show');
+    Route::get('courses', [CourseController::class, 'matriculados'])->name('courses.matriculados');
+});
 
-//Ruta del carrito de la compra. En que grupo de rutas va?
 Route::get('cart', [CartController::class, 'index'])->name('cart.index');
 
-
-//Rutas de matriculación de usuarios, método enrolled responsable matriculación
-//Ruta de Matriculación
 Route::post('courses/{course}/enrolled', [CourseController::class, 'enrolled'])
-->middleware('auth')->name('course.enrolled');
+    ->middleware('auth')->name('course.enrolled');
 
-//Ruta para el control de avance del curso
 Route::get('course-status/{course}', function (Course $course) {
     return view('courses.status', ['course' => $course]); 
 })->name('courses.status')->middleware('auth');
-
-// Route::get('courses-status/{course}', [CourseController::class, 'status'])
-// ->name('courses.status');
-
-
-
 
 
 
