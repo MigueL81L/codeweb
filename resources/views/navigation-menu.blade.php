81e46b51489f98f7dbbe970054e54b529c17c357
Migue81 <81migue81@gmail.com>
2024-10-29 07:58:37 +0100

    fotoperfil5

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -120,7 +120,8 @@
                     @auth
                             <x-dropdown align="right" width="48">
                                 <x-slot name="trigger">
-                                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    {{-- @if (Laravel\Jetstream\Jetstream::managesProfilePhotos()) --}}
+                                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos() && Auth::user()->profile_photo_path)
                                         <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition ml-4">
                                             {{-- <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
                                             <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('storage/app/public/' . Auth::user()->profile_photo_path) }}" alt="{{ Auth::user()->name }}" />
@@ -246,7 +247,8 @@
                         @auth
                                 <x-dropdown align="right" width="48">
                                     <x-slot name="trigger">
-                                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                        {{-- @if (Laravel\Jetstream\Jetstream::managesProfilePhotos()) --}}
+                                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos() && Auth::user()->profile_photo_path)
                                             <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                                 {{-- <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
                                                 <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('storage/app/public/' . Auth::user()->profile_photo_path) }}" alt="{{ Auth::user()->name }}" />
