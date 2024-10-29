6bda1646a46b8c73367deba596a2318349aa5006
Migue81 <81migue81@gmail.com>
2024-10-29 08:16:02 +0100

    fotoperfil6

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -128,12 +128,17 @@
                                         </button>
                                     @else
                                         <span class="inline-flex rounded-md">
-                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150  ml-4">
+                                            {{-- <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150  ml-4">
                                                 {{ Auth::user()->name }}
 
                                                 <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                                 </svg>
+                                            </button> --}}
+                                            <button class="flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition ml-4">
+                                                <div class="h-8 w-8 bg-gray-200 rounded-full flex items-center justify-center">
+                                                    <span class="font-semibold text-gray-600">{{ strtoupper(substr(Auth::user()->name, 0, 1)) }}</span>
+                                                </div>
                                             </button>
                                         </span>
                                     @endif
@@ -255,12 +260,17 @@
                                             </button>
                                         @else
                                             <span class="inline-flex rounded-md">
-                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                                {{-- <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                                     {{ Auth::user()->name }}
     
                                                     <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                                     </svg>
+                                                </button> --}}
+                                                <button class="flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition ml-4">
+                                                    <div class="h-8 w-8 bg-gray-200 rounded-full flex items-center justify-center">
+                                                        <span class="font-semibold text-gray-600">{{ strtoupper(substr(Auth::user()->name, 0, 1)) }}</span>
+                                                    </div>
                                                 </button>
                                             </span>
                                         @endif
