a928640a926104550c255b6629b6134adacc51a7
Migue81 <81migue81@gmail.com>
2024-10-29 07:43:02 +0100

    fotoperfil3

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -123,7 +123,7 @@
                                     @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                         <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition ml-4">
                                             {{-- <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
-                                            <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('storage/profile-photos/' . Auth::user()->profile_photo_path) }}" alt="{{ Auth::user()->name }}" />
+                                            <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('storage/app/public/' . Auth::user()->profile_photo_path) }}" alt="{{ Auth::user()->name }}" />
                                         </button>
                                     @else
                                         <span class="inline-flex rounded-md">
@@ -249,7 +249,7 @@
                                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                             <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                                 {{-- <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
-                                                <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('storage/profile-photos/' . Auth::user()->profile_photo_path) }}" alt="{{ Auth::user()->name }}" />
+                                                <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('storage/app/public/' . Auth::user()->profile_photo_path) }}" alt="{{ Auth::user()->name }}" />
                                             </button>
                                         @else
                                             <span class="inline-flex rounded-md">
