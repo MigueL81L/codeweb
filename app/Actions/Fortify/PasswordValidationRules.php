eace9ff83d6967c376890853611fecf085ad7e06
Migue81 <81migue81@gmail.com>
2024-11-20 20:02:21 +0100

    accesibilidad

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -11,10 +11,6 @@ trait PasswordValidationRules
      *
      * @return array<int, \Illuminate\Contracts\Validation\Rule|array<mixed>|string>
      */
-    // protected function passwordRules(): array
-    // {
-    //     return ['required', 'string', Password::default(), 'confirmed'];
-    // }
 
     protected function passwordRules(): array
     {
--- resources/views/externos/accessibility-statement.blade.php
+++ resources/views/externos/accessibility-statement.blade.php
@@ -9,7 +9,7 @@
                 <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
                     <h1 class="text-3xl font-bold mb-4 text-center">Declaración de Accesibilidad</h1> <!-- Título de sección -->
 
-                    <h2 class="mt-4 text-xl font-bold">Compromiso de accesibilidad</h2>
+                    <h2 class="mt-4 text-xl font-bold text-center">Compromiso de accesibilidad</h2>
                     <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
                         En CodeWeb, nuestra misión es proporcionar acceso a contenido educativo de calidad para todos. Reconocemos la importancia de la accesibilidad y trabajamos continuamente para mejorar la experiencia de uso para todos nuestros estudiantes, independientemente de sus habilidades.
                     </p>
@@ -18,7 +18,7 @@
                         Creemos firmemente que la accesibilidad es un aspecto fundamental de nuestros servicios y queremos asegurar que todos los individuos tengan la oportunidad de aprender y desarrollarse en nuestra plataforma.
                     </p>
 
-                    <h2 class="mt-4 text-xl font-bold">Nuestras mejoras en accesibilidad</h2>
+                    <h2 class="mt-4 text-xl font-bold text-center">Nuestras mejoras en accesibilidad</h2>
                     <ul class="mt-2 text-gray-700 dark:text-gray-300 text-left list-disc list-inside">
                         <li><strong>Directrices de accesibilidad:</strong> Seguimos las Pautas de Accesibilidad para el Contenido Web 2.1 (WCAG) del World Wide Web Consortium (W3C) y aplicamos las especificaciones de accesibilidad en aplicaciones web enriquecidas (WAI-ARIA).</li>
                         <li><strong>Pruebas de compatibilidad:</strong> Comprobamos la compatibilidad de nuestro sitio web y aplicaciones móviles con diversas herramientas de accesibilidad y lectores de pantalla.</li>
@@ -30,7 +30,7 @@
                         <li><strong>Investigación continua:</strong> Trabajamos con consultores de accesibilidad para realizar auditorías y recibir recomendaciones sobre cómo mejorar continuamente.</li>
                     </ul>
 
-                    <h2 class="mt-4 text-xl font-bold">Asistencia en materia de accesibilidad</h2>
+                    <h2 class="mt-4 text-xl font-bold text-center">Asistencia en materia de accesibilidad</h2>
                     <p class="mt-2 text-gray-700 dark:text-gray-300 text-left">
                         Si un usuario tiene problemas al utilizar nuestra plataforma o desea compartir sugerencias sobre cómo mejorar la accesibilidad de nuestros servicios, puede contactarnos a través de nuestra dirección de correo electrónico: <a href="mailto:accessibility@codeweb.com" class="text-blue-600 hover:underline">accessibility@codeweb.com</a> o utilizar la opción de asistencia disponible en nuestra plataforma.
                     </p>
