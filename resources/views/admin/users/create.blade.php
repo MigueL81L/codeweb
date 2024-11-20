fe1705dfe025b04e417b523a6093caeb6ea12192
Migue81 <81migue81@gmail.com>
2024-11-20 20:18:45 +0100

    cancelarUser2

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -73,7 +73,7 @@
                     <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Crear Usuario</button>
                     
                     <!-- Botón de Cancelar -->
-                    <a href="{{ route('admin.users.index') }}" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mt-2 inline-block">Cancelar</a>
+                    <button type="button" onclick="window.location='{{ route('admin.users.index') }}'" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mt-2">Cancelar</button>
                 </div>
             </form>
         </div>
