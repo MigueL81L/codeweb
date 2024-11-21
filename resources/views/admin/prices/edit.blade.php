1e9765b9385ddf2bb01c29bbad22ea522ea4bb0f
Migue81 <81migue81@gmail.com>
2024-11-21 18:28:41 +0100

    responsiveCancelarEditPrice

--- resources/views/admin/prices/edit.blade.php
+++ resources/views/admin/prices/edit.blade.php
@@ -34,12 +34,12 @@
                 </div>
 
                 
-                <div class="mb-4">
+                <div class="mb-4 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
                     <!-- Botón de Enviar -->
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Actualizar Precio</button>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2 sm:mb-0">Actualizar Precio</button>
 
                     <!-- Botón de Cancelar -->
-                    <button type="button" onclick="window.location='{{ route('admin.prices.index') }}'" class="btn btn-danger ml-2">Cancelar</button>
+                    <button type="button" onclick="window.location='{{ route('admin.prices.index') }}'" class="btn btn-danger ml-0 sm:ml-2">Cancelar</button>
                 </div>
             </form>
         </div>
