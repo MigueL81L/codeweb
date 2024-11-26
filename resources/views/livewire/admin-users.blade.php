89337a4c19dd3b30368318a7417a4484ca09a9f3
Migue81 <81migue81@gmail.com>
2024-11-26 19:44:47 +0100

    roles3

--- resources/views/livewire/admin-users.blade.php
+++ resources/views/livewire/admin-users.blade.php
@@ -31,7 +31,7 @@
                             </button>
 
                             <!-- Botón Reset para Filtrar por Roles -->
-                            <button type="button" wire:click="resetRoles" class="btn btn-danger btn-block mt-2">
+                            <button type="button" wire:click="resetRoles" class="bg-gray-500 hover:bg-gray-600 text-white font-bold h-10 py-2 px-4 rounded w-full sm:w-auto">
                                 <i class="fas fa-undo text-xs mr-2"></i>
                                 Reset Roles
                             </button>
