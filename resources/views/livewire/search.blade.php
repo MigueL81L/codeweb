429ab7994da809ab1c404b5b656faf90bbc7c5f0
Migue81 <81migue81@gmail.com>
2024-11-27 15:24:46 +0100

    search

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,8 +2,7 @@
     <input wire:model.live="search" class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
            type="search" name="search" placeholder="Search">
 
-    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 
-            rounded absolute right-0 top-0 mt-2">
+    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded absolute right-0 top-0 mt-2">
         Reset
     </button>
 
