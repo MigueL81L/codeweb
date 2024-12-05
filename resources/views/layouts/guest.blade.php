3b0b2b4f54b271c6c4194ee0a5e66bbb7a6185c5
Migue81 <81migue81@gmail.com>
2024-12-05 17:00:40 +0100

    favicon authentification views

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('public/img/logo/Logo64.png') }}" type="image/png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
